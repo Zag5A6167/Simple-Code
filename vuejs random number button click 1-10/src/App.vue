afe0b4efe4d1d24aaf00eeb3b514f341eca97b11
Zag <za12ew44zz_999@hotmail.com>
2024-11-03 08:07:44 +0700

    add

---
+++ vuejs random number button click 1-10/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+  import { ref } from 'vue'
+
+  const n = ref(0)
+
+  const increment = () =>{
+    n.value = Math.floor(Math.random() * 10)+1
+  }
+  console.log(n.value)
+</script>
+
+
+<template>
+  <div>{{ n }}</div>
+  <button @click="increment">Click Radom</button>
+</template>
