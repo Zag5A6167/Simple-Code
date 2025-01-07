980e4a45234389a2dffbf73e1697804ae52c6b8c
Zag Chakrit <85066044+Zag5A6167@users.noreply.github.com>
2025-01-07 15:21:19 +0700

    Add files via upload

---
+++ setArrayToZero.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <set>
+#include <cctype>
+#include<cmath>
+#include<typeinfo>
+#include<vector>
+#include<algorithm>
+#include<utility>
+#include<string>
+using namespace std;
+
+
+#define F first
+#define S second
+#define ll long long
+#define nl "\n"
+#define PB push_back
+
+
+class Solution {
+public:
+    
+};
+
+
+int main() {    
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int arr[10] = {0};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    
+
+
+
+    for(int i = 0 ; i < n ; i++){
+        cout << arr[i] << " " ;
+    }
+
+
+
+
+
+
+
+
+        return 0;
+}
