0f118e1b6f92e77aaac824b6d7ffb20d5e9bab62
Zag Chakrit <85066044+Zag5A6167@users.noreply.github.com>
2025-01-09 23:15:34 +0700

    Add files via upload

---
+++ struct.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <set>
+#include <cctype>
+#include<cmath>
+#include<typeinfo>
+#include<vector>
+#include<algorithm>
+#include<utility>
+#include<string>
+using namespace std;
+
+
+#define F first
+#define S second
+#define ll long long
+#define nl "\n"
+#define PB push_back
+
+
+class Solution {
+public:
+    
+};
+
+struct student{
+    string name;
+};
+
+int main() {    
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+   
+    student s1;
+    s1.name = "W";
+
+    cout << s1.name;
+
+
+
+
+
+
+
+
+        return 0;
+}
