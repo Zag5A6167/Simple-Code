e14f12c13e9355d27ef200cf7e73d61cb058a0a3
Zag Chak <85066044+Zag5A6167@users.noreply.github.com>
2025-01-20 07:47:16 +0700

    Add files via upload

---
+++ Size_of_fundamental_datatype.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+   
+  
+    cout << "int    "<< sizeof(int) << "\n";
+    cout << "unsigned int   "<<sizeof(unsigned int) << "\n";
+    cout << "short int  "<<sizeof(short int)<< "\n";
+    cout << "long int   "<<sizeof(long int)<< "\n";
+    cout << "long long int  "<<sizeof(long long int)<< "\n";
+    cout << "char   "<<sizeof(char)<< "\n";
+    cout << "float  "<<sizeof(float)<< "\n";
+    cout << "double "<<sizeof(double)<< "\n";
+    cout << "bool   "<<sizeof(bool)<< "\n";
+   
+
+
+    return 0;
+}
